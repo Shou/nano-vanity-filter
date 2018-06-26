70eb32b8e57676f4f17be58433ab7dba2e3bdbb7
Benedict Aas <x@shou.io>
2018-06-26 22:00:35 +0100

    refactor: use hashtable for 10x speedup

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,12 +8,13 @@
              ScopedTypeVariables,
              RankNTypes,
              AllowAmbiguousTypes,
-             KindSignatures
+             KindSignatures,
+             TypeOperators,
+             TypeFamilies
 #-}
 
 module Lib where
 
-import qualified Crypto.Hash as Hash
 import Control.Monad
 import qualified Control.Monad.STM as STM
 import Control.Concurrent
@@ -22,6 +23,8 @@ import qualified Control.Concurrent.MVar as Mut
 import qualified Control.Exception as Except
 import qualified Control.Lens as Lens
 import Control.Monad
+import Control.Type.Operator
+import qualified Crypto.Hash as Hash
 import qualified Crypto.ECC.Edwards25519 as Ecced
 import qualified Crypto.Error as Cerr
 import qualified Crypto.PubKey.Ed25519 as Ced
@@ -31,11 +34,14 @@ import qualified Data.ByteArray as BArray
 import qualified Data.ByteString as Bytes
 import qualified Data.ByteString.Char8 as Bytes8
 import Data.Coerce
+import qualified Data.HashTable.IO as HashT
 import qualified Data.HexString as Hex
+import Data.Int
 import Data.List
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Monoid
+import Data.Proxy
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as TEnc
@@ -58,24 +64,30 @@ data Wallet =
 
 instance Show Wallet where
   show (Wallet seed priv pub addr)
-    = "Wallet \n\tSeed " <> showAsHEX seed <> "\n\t" <> show (Bytes.unpack seed)
-      <> "\n\tPrivate key " <> showAsHEX priv <> "\n\t" <> show (Bytes.unpack priv)
-      <> "\n\tPublic key " <> showAsHEX pub <> "\n\t" <> show (Bytes.unpack pub)
-      <> "\n\tAddress " <> Bytes8.unpack addr <> "\n\t" <> show (Bytes.unpack addr)
+    = "Wallet \n\tSeed " <> showAsHEX seed -- <> "\n\t" <> show (Bytes.unpack seed)
+      <> "\n\tPrivate key " <> showAsHEX priv -- <> "\n\t" <> show (Bytes.unpack priv)
+      <> "\n\tPublic key " <> showAsHEX pub -- <> "\n\t" <> show (Bytes.unpack pub)
+      <> "\n\tAddress " <> Bytes8.unpack addr -- <> "\n\t" <> show (Bytes.unpack addr)
 
 data Match =
-  Match { getMatchWords :: Set.Set Bytes.ByteString
+  Match { getMatchWords :: Vector.Vector Bytes.ByteString
         , getMatchSeed :: Bytes.ByteString
         , getMatchAddr :: Bytes.ByteString
         }
 
 instance Show Match where
   show (Match ws sd ad) = "Match "
-    <> (Bytes8.unpack $ Bytes.intercalate ", " $ Set.toList ws)
+    <> (Bytes8.unpack $ Bytes.intercalate ", " $ Vector.toList ws)
     <> "\n\t"
     <> showAsHEX sd
     <> "\n\t"
     <> Bytes8.unpack ad
+
+type HashSet k = HashT.BasicHashTable k ()
+
+
+setLookup :: (Eq k, _) => HashT.BasicHashTable k () -> k -> IO $ Maybe k
+setLookup set word = maybe Nothing (Just . const word) <$> HashT.lookup set word
 
 showAsHEX = Text.unpack . Text.toUpper . Hex.toText . Hex.fromBytes
 hexToBytes = Hex.toBytes . Hex.hexString . TEnc.encodeUtf8 . Text.toLower
@@ -103,6 +115,9 @@ nanoBase32' = Bytes.foldl' go (0, 0)
           word8 = Bytes.singleton $ nanoAlphabet `Bytes.index` bit5
       in (newCarry, acc <> word8) -}
 
+englishDict :: HashSet Bytes.ByteString
+englishDict = unsafePerformIO HashT.new
+
 
 -- Utils
 
@@ -113,19 +128,22 @@ nonAlphabet c = c < 76 || c > 122
 
 -- / Utils
 
--- XXX(Shou): ideally we'd just combine the two functions below by passing (bitlen :: Nat)
-hash256 :: Bytes.ByteString -> Bytes.ByteString
-hash256 = BArray.convert . Hash.hash @_ @(Hash.Blake2b 256)
-
-hash512 :: Bytes.ByteString -> Bytes.ByteString
-hash512 = BArray.convert . Hash.hash @_ @(Hash.Blake2b 512)
+-- | Hasher function that takes a bit length as a proxy argument, e.g.
+--
+-- > hash (Proxy :: Proxy (Blake2b 256))
+--
+-- or with TypeApplications
+--
+-- > hash (Proxy @(Blake2b 256))
+hash :: forall hasher. Hash.HashAlgorithm hasher => Proxy hasher -> Bytes.ByteString -> Bytes.ByteString
+hash _ = BArray.convert . Hash.hash @_ @hasher
 
 makeNanoPriv :: Bytes.ByteString -> Bytes.ByteString
-makeNanoPriv = hash256
+makeNanoPriv = hash $ Proxy @(Hash.Blake2b 256)
 
 makeNanoPubkey :: Bytes.ByteString -> Bytes.ByteString
 makeNanoPubkey privkey =
-  let hashedPrivkey = Bytes.take 32 $ hash512 privkey
+  let hashedPrivkey = Bytes.take 32 $ hash (Proxy @(Hash.Blake2b 512)) privkey
       clampedHash0 = over hashedPrivkey 0 (.&. 248)
       clampedHash = over clampedHash0 31 (\n -> n .&. 127 .|. 64)
       scalar = Cerr.throwCryptoError . Ecced.scalarDecodeLong $ clampedHash
@@ -137,8 +155,9 @@ makeNanoAddress bs = TEnc.encodeUtf8 $ prefix <> account <> checksum
   where
     paddedBs = if Bytes.head bs < 128 then 0 `Bytes.cons` bs else bs
     account = Base32.toText $ nanoBase32 paddedBs
-    checksum = Base32.toText $ nanoBase32 $ Bytes.reverse $ Bytes.take 5 $ hash256 bs
+    checksum = Base32.toText $ nanoBase32 $ Bytes.reverse $ hash (Proxy @(Hash.Blake2b 40)) bs
 
+makeNanoWalletFromTextSeed :: Text.Text -> Wallet
 makeNanoWalletFromTextSeed textSeed =
   let byteSeed = hexToBytes textSeed
   in makeNanoWalletFromSeed byteSeed
@@ -154,67 +173,59 @@ newNanoWallet = do
   byteSeed <- Bytes.pack <$> replicateM 32 (Rand.randomIO @Word.Word8)
   return $ makeNanoWalletFromSeed byteSeed
 
-generateNanoVanities :: STM.TQueue (Set.Set Bytes.ByteString, Wallet)
-                     -> Map.Map Word.Word8 (Set.Set Bytes.ByteString)
+generateNanoVanities :: STM.TQueue (Vector.Vector Bytes.ByteString, Wallet)
                      -> Int
                      -> IO ()
-generateNanoVanities queue englishDict score = Except.catch
-  (forever $ newNanoWallet >>= printScore queue englishDict score)
-  (const @_ @Except.SomeException $ generateNanoVanities queue englishDict score)
+generateNanoVanities queue score = Except.catch
+  (forever $ newNanoWallet >>= printScore queue score)
+  (const @_ @Except.SomeException $ generateNanoVanities queue score)
 
-vanityMatchQuantity :: _ -> Bytes.ByteString -> Set.Set Bytes.ByteString
-vanityMatchQuantity englishDict addr = containsWord mempty $ Bytes.drop (Text.length prefix + 1) addr
+vanityMatchQuantity :: Bytes.ByteString -> IO $ Vector.Vector Bytes.ByteString
+vanityMatchQuantity addr = containsWord mempty $ Bytes.drop (Text.length prefix + 1) addr
   where
-    containsWord :: Set.Set Bytes.ByteString -> Bytes.ByteString -> Set.Set Bytes.ByteString
-    containsWord wordAcc addrTail =
-      let firstLetter = Bytes.head addrTail
-          wordSet = fromMaybe mempty $ Map.lookup firstLetter englishDict
-          matcher = flip Bytes.isPrefixOf addrTail
-          match = find matcher wordSet
-      in case () of
-              -- We require words to be at least 4 chars long
-           _ | Bytes.length addrTail < 4 -> wordAcc
-              -- We don't want to try to match non-alphabetical characters
-             | Just n <- Bytes.findIndex nonAlphabet (Bytes.take 4 addrTail) ->
-                 containsWord wordAcc $ Bytes.drop (n + 1) addrTail
-             | otherwise ->
-                 maybe (containsWord wordAcc $ Bytes.tail addrTail)
-                       (\word -> containsWord (Set.insert word wordAcc) $ Bytes.drop (Bytes.length word) addrTail)
-                       match
+    containsWord :: Vector.Vector Bytes.ByteString -> Bytes.ByteString -> IO $ Vector.Vector Bytes.ByteString
+    containsWord wordAcc addrTail = case () of
+        -- We require words to be at least 4 chars long
+      _ | Bytes.length addrTail < 4 -> return wordAcc
 
-printScore :: STM.TQueue (Set.Set Bytes.ByteString, Wallet)
-           -> Map.Map Word.Word8 (Set.Set Bytes.ByteString)
+        -- We don't want to try to match non-alphabetical characters
+        | Just n <- Bytes.findIndex nonAlphabet (Bytes.take 4 addrTail) ->
+           containsWord wordAcc $ Bytes.drop (n + 1) addrTail
+
+        | otherwise -> do
+            -- Get the longest matching word
+            let possibleWords = [ Bytes.take n addrTail | n <- [9, 8 .. 4] ]
+            maybeMatch <- listToMaybe . catMaybes <$> mapM (setLookup englishDict) possibleWords
+            maybe (containsWord wordAcc $ Bytes.tail addrTail)
+                  (\word -> containsWord (Vector.snoc wordAcc word) $ Bytes.drop (Bytes.length word) addrTail)
+                  maybeMatch
+
+printScore :: STM.TQueue (Vector.Vector Bytes.ByteString, Wallet)
            -> Int
            -> Wallet
            -> IO ()
-printScore queue englishDict score wallet = do
+printScore queue score wallet = do
   incQuantity
-  let matches = vanityMatchQuantity englishDict $ getAddress wallet
+  matches <- vanityMatchQuantity $ getAddress wallet
   when (length matches > score) $ STM.atomically $ STM.writeTQueue queue (matches, wallet)
 
 runPrinter queue = forever $ do
   (matches, wallet) <- STM.atomically $ STM.readTQueue queue
   print $ Match matches (getSeed wallet) (getAddress wallet)
-  print (Bytes.unpack $ getSeed wallet, "seed")
-  print (Bytes.unpack $ getPrivateKey wallet, "secret")
-  print (Bytes.unpack $ getPublicKey wallet, "shared")
 
 incMVar :: Mut.MVar Integer
 incMVar = unsafePerformIO $ Mut.newMVar 0
 incQuantity = Mut.modifyMVar_ incMVar increment
   where
+    -- XXX(Shou): strict increment to remove space leaks when score isn't shown
     increment !n = return $! n + 1
 
-wordsToDict :: [Bytes.ByteString] -> Map.Map Word.Word8 (Set.Set Bytes.ByteString)
-wordsToDict = foldl' go mempty
-  where
-    go accMap word =
-      let firstLetter = Bytes.head word
-          inserter wordListMay = Just . Set.insert word $ fromMaybe mempty wordListMay
-      in Map.alter inserter firstLetter accMap
+wordsToDict :: [Bytes.ByteString] -> IO ()
+wordsToDict = mapM_ $ \word -> HashT.insert englishDict word ()
 
-loadEnglishDict :: FilePath -> IO _
-loadEnglishDict = (wordsToDict . filter predicates . splitNewlines <$>) . Bytes.readFile
+-- TODO(Shou): return length of longest word so we know the upper boundary when searching, instead of 9
+loadEnglishDict :: FilePath -> IO ()
+loadEnglishDict = wordsToDict <=< (filter predicates . splitNewlines <$>) . Bytes.readFile
   where
     splitNewlines :: Bytes.ByteString -> [Bytes.ByteString]
     splitNewlines = Bytes.split 10
@@ -232,7 +243,7 @@ printSpeed n = do
 someFunc :: () -> IO ()
 someFunc options = do
     printerQueue <- STM.newTQueueIO
-    englishDict <- loadEnglishDict americanEnglishFilename
+    loadEnglishDict americanEnglishFilename
 
     forkIO $ printSpeed 1
 
@@ -240,7 +251,7 @@ someFunc options = do
 
     threadCount <- getNumProcessors
     forM_ @[] @IO @Int [1 .. threadCount] $ \workerNumber -> forkIO $ do
-      generateNanoVanities printerQueue englishDict minScore
+      generateNanoVanities printerQueue minScore
 
     runPrinter printerQueue
 
